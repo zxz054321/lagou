d677bc5b1fd85c1d67cf3bb9b34ef7ba18e0053a
Abel <zxz054321@foxmail.com>
2019-10-31 11:03:50 +0800

    support multiple jobs

--- app/Console/Commands/CrawlCommand.php
+++ app/Console/Commands/CrawlCommand.php
@@ -16,7 +16,9 @@ class CrawlCommand extends Command
     /**
      * The console command description.
      */
-    protected $description = '爬取拉勾广州所有PHP职位';
+    protected $description = '抓取广州的技术职位';
+
+    protected $jobs = ['PHP', 'Java', 'Go', 'python', 'Ruby'];
 
     /**
      * Execute the console command.
@@ -28,31 +30,34 @@ class CrawlCommand extends Command
             return;
         }
 
-        $positionData = $this->runCurlCommand();
+        foreach ($this->jobs as $job) {
+            $positionData = $this->runCurlCommand($job);
+            $this->info("拉取到 $job 职位的分页数据，共 {$positionData->content->pageSize} 页");
 
-        for ($i = 1; $i <= $positionData->content->pageSize; $i++) {
-            $this->output->newLine();
-            $this->randomSleep();
-            $this->info("正在获取第 $i 页职位数据……");
+            for ($i = 1; $i <= $positionData->content->pageSize; $i++) {
+                $this->output->newLine();
+                $this->randomSleep();
+                $this->info("正在获取第 $i 页职位数据……");
 
-            $positionData = $this->runCurlCommand($i);
+                $positionData = $this->runCurlCommand($i);
 
-            foreach ($positionData->content->positionResult->result as $position) {
-                $position->updateTime = now();
+                foreach ($positionData->content->positionResult->result as $position) {
+                    $position->updateTime = now();
 
-                LagouPosition::updateOrCreate(['positionId' => $position->positionId], (array) $position);
+                    LagouPosition::updateOrCreate(['positionId' => $position->positionId], (array) $position);
+                }
             }
         }
     }
 
-    protected function runCurlCommand(int $page = 1): \stdClass
+    protected function runCurlCommand(string $job, int $page = 1): \stdClass
     {
         // 更换 cookie 不再影响缓存
-        $key = 'lagou:ajax:position:' . md5($this->buildCurlCommand('', $page));
+        $key = 'lagou:ajax:position:' . md5($this->buildCurlCommand($job, '', $page));
 
-        return Cache::remember($key, now()->addHour(), function () use ($page) {
+        return Cache::remember($key, now()->addHours(12), function () use ($job, $page) {
             $cookie  = $this->option('cookie');
-            $command = $this->buildCurlCommand($cookie, $page);
+            $command = $this->buildCurlCommand($job, $cookie, $page);
             $data    = json_decode(exec($command));
 
             if (is_null($data)) {
@@ -69,9 +74,9 @@ class CrawlCommand extends Command
         });
     }
 
-    protected function buildCurlCommand(string $cookie, int $page): string
+    protected function buildCurlCommand(string $job, string $cookie, int $page): string
     {
-        return "curl 'https://www.lagou.com/jobs/positionAjax.json?px=new&city=%E5%B9%BF%E5%B7%9E&needAddtionalResult=false' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Origin: https://www.lagou.com' -H 'X-Anit-Forge-Code: 0' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Anit-Forge-Token: None' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://www.lagou.com/jobs/list_PHP?px=new&city=%E5%B9%BF%E5%B7%9E' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' -H 'Cookie: {$cookie}' --data 'first=true&pn=$page&kd=PHP' --compressed";
+        return "curl 'https://www.lagou.com/jobs/positionAjax.json?px=new&city=%E5%B9%BF%E5%B7%9E&needAddtionalResult=false' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Origin: https://www.lagou.com' -H 'X-Anit-Forge-Code: 0' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'X-Requested-With: XMLHttpRequest' -H 'X-Anit-Forge-Token: None' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://www.lagou.com/jobs/list_{$job}?px=new&city=%E5%B9%BF%E5%B7%9E' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' -H 'Cookie: {$cookie}' --data 'first=true&pn=$page&kd={$job}' --compressed";
     }
 
     protected function randomSleep(): void
